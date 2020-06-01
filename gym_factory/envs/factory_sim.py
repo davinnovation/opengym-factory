2597c14dd59efaecab6360457674770dca8a5b44
dong heon cho <davinnovation@gmail.com>
2020-06-02 00:33:27 +0900

    add yield

--- gym_factory/envs/factory_sim.py
+++ gym_factory/envs/factory_sim.py
@@ -47,12 +47,12 @@ class PRT(TIKTOK):
             return self.sch_table[:, self.cur_idx-self.prodct_time]
         else: return [0] * self.sch_table.shape[0]
 
-class MOL(TIKTOK):
+class MOL_BLK(TIKTOK):
     def __init__(self, times:int, products:int,
         daily_consts:list, tik_const:float=6.667,
         prodct_time:int=48, failure_ratio:float=1-97.5,
         ):
-        super(MOL, self).__init__(times)
+        super(MOL_BLK, self).__init__(times)
 
         # state
         self.sch_table = np.zeros((products, times))
@@ -60,11 +60,31 @@ class MOL(TIKTOK):
         # const
         self.daily_consts = daily_consts
         self.tik_const = tik_const
-        self.product_time = prodct_time
+        self.prodct_time = prodct_time
         self.failure_ratio = failure_ratio
+
+        self.monthly_cut_yield = []
     
     def table2status(self):
-        pass
+        ret_stat = []
+        STATUS = ('CHECK', 'PROCESS', 'CHANGE', 'STOP')
+        for i in range(self.sch_table.shape[1]):
+            if sum(self.sch_table[:, i].flatten()) == 0:
+                ret_stat.append([STATUS[0], STATUS[-1]])
 
     def const_check(self):
-        # 1. 
+        # 1. by time
+        for i in range(self.times):
+            if sum(self.sch_table[i].flatten()) > self.tik_const:
+                return False
+        # 2. by day
+        for i in range(self.times/24):
+            if sum(self.sch_table[i:i*24].flatten()) > self.daily_consts[i]:
+                return False
+        return True
+
+    def production(self):
+        if self.cur_idx >= self.prodct_time:
+            # in this simulation, failure ratio is 0% for ease
+            return self.sch_table[:, self.cur_idx-self.prodct_time] * self.failure_ratio * [506, 506, 400, 400] * self.monthly_cut_yield[self.cur_idx]
+        else: return [0] * self.sch_table.shape[0]
